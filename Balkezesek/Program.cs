062163a65cad98bb08ae416cc8e1688ab33381a0
bettilaszlo <laszlobetti3@gmail.com>
2020-10-16 11:44:28 +0200

    Add project files.

---
+++ Balkezesek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static List<balkezesek> balkez = new List<balkezesek>();
+        static void Beolvasás()
+        {
+            StreamReader file = new StreamReader("balkezesek.csv");
+            file.ReadLine();
+
+            while (!file.EndOfStream)
+            {
+                string[] a = file.ReadLine().Split(';');
+                balkez.Add(new balkezesek(a[0], a[1], a[2], Convert.ToInt32(a[3]), Convert.ToInt32(a[4])));
+            }
+            file.Close();
+
+        }
+        static void HarmadikFeladat()
+        {
+            Console.WriteLine("3. feladat: ", balkez.Count);
+        }
+        static void Main(string[] args)
+        {
+            Beolvasás();
+            HarmadikFeladat();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Balkezesek/balkezesek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class balkezesek
+    {
+        public string Nev { get; private set; }
+        public string Elso { get; private set; }
+        public string Utolso { get; private set; }
+        public int Suly { get; private set; }
+        public int Magassag { get; private set; }
+        
+        public balkezesek(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.Nev = nev;
+            this.Elso = elso;
+            this.Utolso = utolso;
+            this.Suly = Convert.ToInt32(suly);
+            this.Magassag = Convert.ToInt32(magassag);
+        }
+    }
+}
